f0168bc585d213da288d904f4c23bdeac9bf02f1
laurabpaulsen <202005791@post.au.dk>
2025-10-29 14:13:35 +0100

    filter outliers from plot

--- h4_phase_modulates_RT.py
+++ h4_phase_modulates_RT.py
@@ -1,5 +1,5 @@
 """
-This script tests H3B:
+This script tests H4:
 
 Respiration has functional relevance reflected in modulation of response time across different phases of the respiratory cycle.
 
@@ -11,6 +11,9 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 import statsmodels.formula.api as smf
+from pyriodic import Circular, CircPlot
+from utils import binned_stats
+
 
 
 # PARAMETERS
@@ -26,6 +29,7 @@ if SUPPRESS_WARNINGS:
     import warnings
     from statsmodels.tools.sm_exceptions import ConvergenceWarning
     warnings.filterwarnings("ignore", category=ConvergenceWarning, module="statsmodels")
+
 
 
 def LMEM(data):
@@ -52,10 +56,84 @@ def LMEM(data):
     return model.fit()
 
 
+def plot_subject_RT_by_phase(data, figpath, filter_outliers=True, num_bins=12, stat="mean"):
+    """
+    Plot response time by phase for each subject in a polar plot.
+
+    Parameters
+    ----------
+    data : dict
+        Dictionary containing data for each subject.
+    sfreq : int
+        Sampling frequency.
+    figpath : Path
+        Path to save the figure.
+    filter_outliers : bool, optional
+        Whether to filter out outliers, by default True.
+    num_bins : int, optional
+        Number of bins for phase, by default 12.
+    stat : str, optional
+        Statistic to compute for each bin, by default "mean". Other options: "median", "std".
+
+    Returns
+    -------
+    None
+        Saves the figure to the specified path.
+    """
+    n_rows = 3
+    n_cols = len(data) // n_rows + (len(data) % n_rows > 0)
+
+    figsize = (n_cols * 4, n_rows * 4)
+
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=figsize, subplot_kw={"projection": "polar"}, sharex=True, sharey=True)
+
+    for i, (subj_id, dat) in enumerate(data.items()):
+
+        #preproc = dat["preproc"]
+        circ = dat["circ"]
+        rt = dat["rt"] 
+
+        # find the indices where rt is not none
+        rt_indices = np.where(~np.isnan(rt))[0]
+        rt = rt[rt_indices]
+
+        targets_with_responses = circ.data[rt_indices-1] # get the phase angle at the target stimuli
+
+        # check that target is in label
+        assert ["target" in label for label in circ.labels[rt_indices-1]], "something went wrong"
+
+        if filter_outliers:
+            # detect outliers
+            z_scores = np.abs(stats.zscore(rt))
+            outliers = np.where(z_scores > OUTLIER_THRESHOLD)[0]
+
+            # remove outliers
+            rt = np.delete(rt, outliers)
+            targets_with_responses = np.delete(targets_with_responses, outliers)
+
+        # binned stats
+        bin_centers, avg_response_times, std_response_times = binned_stats(targets_with_responses, rt, n_bins=num_bins, stat=stat)
+
+        circ_for_plot = Circular(bin_centers)
+        plot = CircPlot(circ_for_plot, group_by_labels=False, ax=axes[i], title=subj_id)
+        plot.add_connected_points(y=avg_response_times, color='forestgreen', alpha=0.5, marker ='o')
+        plot.add_hline(y=np.nanmean(avg_response_times), color='gray', linestyle='--', label=f'{stat.capitalize()} RT')
+
+
+
+    plt.tight_layout()
+
+    if figpath:
+        plt.savefig(figpath, dpi=300)
+
+
+
+
+
 
 if __name__ == "__main__":
 
-    dataset = "before_pilots"
+    dataset = "pilots"
     variables = ["rt", "circ", "intensity"]
     data = load_data(variables, dataset)
 
@@ -64,6 +142,17 @@ if __name__ == "__main__":
 
 
     LMEM_data = pd.DataFrame(columns=["participant", "rt", "cos_phase", "sin_phase", "intensity"])
+
+
+    plot_subject_RT_by_phase(
+        data=data,
+        figpath=figpath / "h4_RT_by_phase_polar.png",
+        filter_outliers=True,
+        num_bins=12,
+        stat="mean"
+    )
+
+    #plot_grand_average_modulation()
         
     for participant, values in data.items():
         # remove nans from rt
