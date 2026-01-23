ea82b987b8371542a704c1a9c1fd9141fb2ef8c4
laurabpaulsen <202005791@post.au.dk>
2026-01-23 13:50:10 +0100

    methods figures

--- methods_figs.py
+++ methods_figs.py
@@ -8,7 +8,6 @@ trigger_mapping = {v: k for k, v in trigger_mapping.items()}
 
 
 def plot_surrogate_procedure(data, figpath):
-    # --- FIGURE SETUP ---
     fig, axes = plt.subplots(
         5, 1, figsize=(8, 9), dpi=300, sharex=True,
         gridspec_kw=dict(hspace=0.6, height_ratios=[1.2, 1, 1, 1, 1])
@@ -30,11 +29,11 @@ def plot_surrogate_procedure(data, figpath):
     
     observed_colour = "forestgreen"
 
-    # --- DEFINE SEGMENTS ---
+    
     segments = [(int(peaks[i]), int(peaks[i + 1])) for i in range(len(peaks) - 1)]
     segment_colors = colors[:len(segments)]
 
-    # --- (1) Original respiration signal ---
+    
     for (start, end), color in zip(segments, segment_colors):
         axes[0].plot(np.arange(start, end),
                      ts[samp_min + start:samp_min + end],
@@ -60,7 +59,6 @@ def plot_surrogate_procedure(data, figpath):
 
 
     # --- (3–5) Surrogates ---
-    n_surrogates = 3
     for i_sur, ax in enumerate(axes[2:], start=1):
         np.random.seed(40 + i_sur)
         shuffled_indices = np.random.permutation(len(segments))
@@ -95,21 +93,123 @@ def plot_surrogate_procedure(data, figpath):
     for ax in axes:
         ax.spines[['top', 'right']].set_visible(False)
 
-    for i, ax in enumerate(axes[2:]):
+    for i, ax in enumerate(axes[1:]):
         ax.set_yticks([0, np.pi, 2 * np.pi])
         ax.set_yticklabels(["0", r"$\pi$", r"2$\pi$"])
         ax.set_ylim([0, 2 * np.pi])
-        # turn spines and ticks gray
+        # turn spines and ticks gray for surrogate plots
+        if i > 0:
+            ax.spines[['left', 'bottom']].set_color("dimgray")
+            ax.tick_params(axis='y', colors="dimgray")
 
-        ax.spines[['left', 'bottom']].set_color("dimgray")
-        ax.tick_params(axis='y', colors="dimgray")
 
-    # --- Final layout ---
     plt.tight_layout()
     plt.subplots_adjust(hspace=0.7)
     plt.savefig(figpath, bbox_inches="tight")
     plt.close(fig)
 
+
+def plot_QUEST_moving_window(circ, figpath=None):
+
+
+    # parameters from analysis
+    # for bins refitting of psychometric function
+    DELTA_CENTER = np.pi/20
+    WIDTH = np.pi/4
+
+
+    circ = circ["target"]
+    metadata = circ.metadata
+
+    # convert series (metadata["n_in_block"]) to integer
+    n_target = metadata["n_in_block"].to_numpy()/4 + metadata["block"].astype(int).to_numpy() * 8
+    intensity = metadata["intensity"].to_numpy()
+    correct = metadata["correct"].astype("float").to_numpy()
+    phase_angle = circ.data
+
+
+    
+    # plotting!!!
+    fig, axes = plt.subplots(2, 1, figsize=(8, 6))
+
+    # QUEST procedure
+
+    # line between all points
+    axes[0].plot(n_target, intensity, color="lightgray", lw=1)
+
+    masks = {
+        "Correct":    (correct == 1),
+        "Incorrect":  (correct == 0),
+        "No response": np.isnan(correct),
+    }
+
+    colors = {
+        "Correct": "forestgreen",
+        "Incorrect": "indianred",
+        "No response": "gray",
+    }
+
+    for label, mask in masks.items():
+        axes[0].scatter(
+            n_target[mask],
+            intensity[mask],
+            color=colors[label],
+            label=label,
+            alpha=1,
+            s=15,
+            zorder=2
+        )
+
+        if label != "No response":
+
+            # moving window
+            axes[1].scatter(
+                    phase_angle[mask],
+                    intensity[mask],
+                    color=colors[label],
+                    label=label,
+                    alpha=1,
+                    s=15
+                )
+
+
+    axes[0].set_xlabel("Target number")
+    axes[0].set_xlim([0, 200])
+    axes[0].legend()
+
+    # set ticks for phase angle plot
+    axes[1].set_xticks([0, np.pi, 2 * np.pi])
+    axes[1].set_xticklabels(["0", r"$\pi$", r"2$\pi$"])
+    axes[1].set_xlabel("Phase angle")
+    axes[1].set_xlim([0, 2 * np.pi])
+
+    for ax in axes:
+        ax.set_ylabel("Intensity (mA)")
+        # remove top and right spines
+        ax.spines[['top', 'right']].set_visible(False)
+
+
+    # draw one example of moving window
+    center = np.pi - DELTA_CENTER * 12
+    window_start = center - WIDTH / 2
+    window_end = center + WIDTH / 2
+
+    # red lines to indicate the window
+    axes[1].axvspan(
+        window_start, window_end, 
+        edgecolor = "red", fill=False, facecolor=None,
+        linewidth=2)
+
+    
+        
+    plt.tight_layout()
+    # adjust hspace
+    plt.subplots_adjust(hspace=0.5)
+
+    plt.savefig(figpath)
+    plt.close(fig)
+    
+    
 
 if __name__ == "__main__":
     outdir = Path(__file__).parent / "results" / "methods_fig"
@@ -117,7 +217,12 @@ if __name__ == "__main__":
 
     variables = ["peaks", "troughs", "event_samples_all", "event_ids_all",
                  "preprocessed", "phase_ts"]
-    dataset = "pilots"
-    data = load_data(variables, dataset)
-    data = data["JES"]
-    plot_surrogate_procedure(data, outdir / "surrogate_procedure.png")
+    
+    data = load_data(variables, "pilots")
+    plot_surrogate_procedure(data["JES"], outdir / "surrogate_procedure.svg")
+
+
+    data = load_data(["circ"], "raw")
+
+    participant_id = list(data.keys())[12]
+    plot_QUEST_moving_window(data[participant_id]["circ"], outdir / "QUEST_moving_window.svg")
